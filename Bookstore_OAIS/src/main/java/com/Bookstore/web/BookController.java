6292f1b3336cd410f4e8c9fa959f6135c2613a82
Olli Salo <olliasalo@outlook.com>
2023-09-11 08:43:05 +0300

    booklist.html added

--- Bookstore_OAIS/src/main/java/com/Bookstore/web/BookController.java
+++ Bookstore_OAIS/src/main/java/com/Bookstore/web/BookController.java
@@ -1,14 +1,21 @@
 package com.Bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.Bookstore.model.BookRepository;
+
 import org.springframework.ui.Model;
 
 @Controller
 public class BookController {
-	@RequestMapping(value = "/index")
-	public String bookSubmit(Model model) {
-		
-		return "index";
+	@Autowired
+	private BookRepository repository;
+	
+	@RequestMapping(value = {"/", "/booklist"})
+	public String bookList(Model model) {
+		model.addAttribute("books", repository.findAll());
+		return "booklist";
 	}
 }
