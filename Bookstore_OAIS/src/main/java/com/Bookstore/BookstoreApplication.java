9611e4af09dacb21963daa15ebbb6fc83a6ec01f
Olli Salo <olliasalo@outlook.com>
2023-09-11 08:19:58 +0300

    JPA support and H2

--- Bookstore_OAIS/src/main/java/com/Bookstore/BookstoreApplication.java
+++ Bookstore_OAIS/src/main/java/com/Bookstore/BookstoreApplication.java
@@ -1,13 +1,38 @@
 package com.Bookstore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.Bookstore.model.Book;
+import com.Bookstore.model.BookRepository;
+
 
 @SpringBootApplication
 public class BookstoreApplication {
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner bookDemo(BookRepository repository) {
+		return (args) -> {
+			log.info("save a couple of students");
+			repository.save(new Book("A Farewell to Arms", "Ernest Hemingway", 1929, "1232323-21", 20.00));
+			repository.save(new Book("Animal Farm", "George Orwell", 1945, "2212343-5", 25.00));
+
+			log.info("fetch all students");
+			for (Book book : repository.findAll()) {
+				log.info(book.toString());
+			}
+
+		};
 	}
 
 }
