da3df45777ca9bd18857e9553cb570c567a28d0d
Olli Salo <olliasalo@outlook.com>
2023-10-04 12:40:07 +0300

    Fixed role issue

--- Bookstore_OAIS/src/main/java/com/Bookstore/web/BookController.java
+++ Bookstore_OAIS/src/main/java/com/Bookstore/web/BookController.java
@@ -49,7 +49,7 @@ public class BookController {
     	return repository.findById(id);
     }   
     
-    @PreAuthorize("hasAuthority('ADMIN')")
+    @PreAuthorize("hasRole('ADMIN')")
 	@RequestMapping(value = "/add")
 	public String addBook(Model model){
 	model.addAttribute("book", new Book());
@@ -57,21 +57,21 @@ public class BookController {
 	return "addbook";
 	}
     
-    @PreAuthorize("hasAuthority('ADMIN')")
+    @PreAuthorize("hasRole('ADMIN')")
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public String save(Book book){
 	repository.save(book);
 	return "redirect:booklist";
 	}
 	
-	@PreAuthorize("hasAuthority('ADMIN')")
+	@PreAuthorize("hasRole('ADMIN')")
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
 	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
 	repository.deleteById(bookId);
 	return "redirect:../booklist";
 	}
 	
-	@PreAuthorize("hasAuthority('ADMIN')")
+	@PreAuthorize("hasRole('ADMIN')")
 	@RequestMapping(value = "/edit/{id}")
 	public String editBook(@PathVariable("id") Long bookId, Model model){
 	model.addAttribute("book", repository.findById(bookId));
